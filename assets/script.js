04c014c1dae00bbf24e5d2c6e5c84520dbf07175
SeifWael999 <seifwael242@gmail.com>
2025-01-30 23:53:10 +0200

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+function showAlert() {
+    alert("Hope You Enjoyed Your Stay");
+}
+
+function newGuest(){
+    alert("New Guest : Get 50% Discount");
+}
+
+function oldGuest(){
+    alert("Old Guest : Get Promocode  : OLD123");
+}
